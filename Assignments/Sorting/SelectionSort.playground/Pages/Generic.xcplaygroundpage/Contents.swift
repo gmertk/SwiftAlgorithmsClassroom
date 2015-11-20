e366ce8e0e7fde83c3b7d70cd7d6a61d011c3b20
Fabio Rodella <frodella@daitangroup.com>
2015-11-20 16:00:04 -0200

    Assignment 1 solution

--- Assignments/Sorting/SelectionSort.playground/Pages/Generic.xcplaygroundpage/Contents.swift
+++ Assignments/Sorting/SelectionSort.playground/Pages/Generic.xcplaygroundpage/Contents.swift
@@ -7,11 +7,32 @@ Turn the function into a generic function so that it can sort any type of array.
 For practicing purposes you may want to rewrite the function from scratch rather than just copy-pasting.
 */
 
+func selectionSort<T: Comparable>(var array: [T]) -> [T] {
+  
+  let length = array.count
+  var indexOfMinimum: Int
+  
+  for var i = 0; i < (length - 1); i++ {
+    
+    indexOfMinimum = i
+    
+    for (j, value) in array.enumerate() where j > i && value < array[indexOfMinimum] {
+      
+      indexOfMinimum = j
+    }
+    
+    if indexOfMinimum != i {
+      
+      swap(&array[i], &array[indexOfMinimum])
+    }
+  }
+  
+  
+  return array
+}
 
-
-
-
-//assert(selectionSort(["c", "a", "b"]).isSorted())
+assert(selectionSort(["c", "a", "b"]).isSorted())
+assert(selectionSort(["c", "a", "a", "b", "a"]).isSorted())
 
 /*:
 [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
--- Assignments/Sorting/SelectionSort.playground/Pages/Pass a Closure.xcplaygroundpage/Contents.swift
+++ Assignments/Sorting/SelectionSort.playground/Pages/Pass a Closure.xcplaygroundpage/Contents.swift
@@ -8,10 +8,33 @@ Also check what `@noescape` means in Swift.
 Since you now use another function to do the comparison, you can remove the `Comparable` constraint.
 */
 
+func selectionSort<T>(var array: [T], @noescape isOrderedBefore: (T, T) -> Bool) -> [T] {
+  
+  let length = array.count
+  var indexToSwap: Int
+  
+  for var i = 0; i < (length - 1); i++ {
     
+    indexToSwap = i
+    
+    for (j, value) in array.enumerate() where j > i && isOrderedBefore(value, array[indexToSwap]) {
+      
+      indexToSwap = j
+    }
+    
+    if indexToSwap != i {
+      
+      swap(&array[i], &array[indexToSwap])
+    }
+  }
+  
+  
+  return array
+}
 
-//assert(selectionSort([3, 1, 2], isOrderedBefore: <).isSorted())
 
+assert(selectionSort([3, 1, 2], isOrderedBefore: <).isSorted())
+assert(selectionSort([3, 1, 2], isOrderedBefore: >).reverse().isSorted())
 
 /*:
 [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
