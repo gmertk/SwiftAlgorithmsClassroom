2e98cc53fc953ae73340e9b7c76725dbe1021a0d
NithinNara <nithin.nara@outlook.com>
2015-11-24 01:28:07 +0530

    Assignment-1 Selection sort solution

--- Assignments/Sorting/SelectionSort.playground/Pages/SequenceType Extension.xcplaygroundpage/Contents.swift
+++ Assignments/Sorting/SelectionSort.playground/Pages/SequenceType Extension.xcplaygroundpage/Contents.swift
@@ -7,10 +7,28 @@ Instead of only sorting arrays, modify your function to sort any type conforming
 
 extension SequenceType {
     
+    func selectionSort(isOrderedBefore: (Generator.Element, Generator.Element) -> Bool) -> [Generator.Element] {
+        var array = Array<Generator.Element>(self)
+        var minIndex:Int
+        let length = array.count
+        
+        for i in 0..<length-1 {
+            minIndex = i
+            for j in i+1..<length {
+                if isOrderedBefore(array[j], array[minIndex]) {
+                    minIndex = j
+                }
+            }
+            if(minIndex != i){
+                swap(&array[minIndex], &array[i])
+            }
+        }
+        return array
+    }
 }
 
-//assert([1, 3, 2].selectionSort(<).isSorted())
-//assert([1: "b", 2: "a"].selectionSort({$0.0 > $1.0}).isSorted({$0.0 > $1.0}))
+assert([1, 3, 2].selectionSort(<).isSorted())
+assert([1: "b", 2: "a"].selectionSort({$0.0 > $1.0}).isSorted({$0.0 > $1.0}))
 
 /*:
 [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
