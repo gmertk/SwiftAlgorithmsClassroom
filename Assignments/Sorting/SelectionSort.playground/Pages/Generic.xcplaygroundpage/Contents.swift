75debce8ca3141ed4617d504958973187c56206d
Mejdi Lassidi <mejdi.dingua@gmail.com>
2015-11-24 21:42:26 +0400

    assignement 1 - solution

--- Assignments/Sorting/SelectionSort.playground/Pages/Generic.xcplaygroundpage/Contents.swift
+++ Assignments/Sorting/SelectionSort.playground/Pages/Generic.xcplaygroundpage/Contents.swift
@@ -8,10 +8,32 @@ For practicing purposes you may want to rewrite the function from scratch rather
 */
 
 
+func selectionSort<X:Comparable>(array:[X]) -> [X] {
+    var index = 0
+    var anArray = array
+    while(!anArray.isSorted()) {
+        let subArray = Array(anArray[index..<anArray.count])
+        let minIndex = indexOfMin(subArray)
+        if index+minIndex < anArray.count && minIndex != 0 {
+            swap(&anArray[index], &anArray[index+minIndex])
+        }
+        index++
+    }
+    return anArray
+}
+
+func indexOfMin<X:Comparable>(array:[X]) ->Int {
+    var indexOfMin = 0
+    for (index,value) in array.enumerate() {
+        if value < array[indexOfMin] {
+            indexOfMin = index
+        }
+    }
+    return indexOfMin
+}
 
 
-
-//assert(selectionSort(["c", "a", "b"]).isSorted())
+assert(selectionSort(["c", "a", "b"]).isSorted())
 
 /*:
 [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
