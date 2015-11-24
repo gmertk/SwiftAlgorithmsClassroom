3e53c3183ef3219f31aa0d2a21664a3a63edf24d
Dave Reed <dave256@mac.com>
2015-11-24 17:32:56 -0500

    assignment 1 completed

--- Assignments/Sorting/SelectionSort.playground/Pages/Generic.xcplaygroundpage/Contents.swift
+++ Assignments/Sorting/SelectionSort.playground/Pages/Generic.xcplaygroundpage/Contents.swift
@@ -6,12 +6,29 @@ Turn the function into a generic function so that it can sort any type of array.
 ****
 For practicing purposes you may want to rewrite the function from scratch rather than just copy-pasting.
 */
+func selectionSort<T:Comparable>(var array: [T]) -> [T] {
+    // You may declare array argument with var keyword so that it is copied.
+    func minPosition(array: [T], start: Int) -> Int {
+        var minPos = start
+        for i in start..<array.count where array[i] < array[minPos] {
+            minPos = i
+        }
+        return minPos
+    }
+
+    for i in 0..<array.count {
+        let minIndex = minPosition(array, start: i)
+        if i != minIndex {
+            swap(&array[i], &array[minIndex])
+        }
+    }
+
+    return array
+}
 
 
-
-
-
-//assert(selectionSort(["c", "a", "b"]).isSorted())
+assert(selectionSort(["c", "b", "d", "a", "A"]).isSorted())
+assert(selectionSort(["c", "a", "b"]).isSorted())
 
 /*:
 [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
--- Assignments/Sorting/SelectionSort.playground/Pages/Pass a Closure.xcplaygroundpage/Contents.swift
+++ Assignments/Sorting/SelectionSort.playground/Pages/Pass a Closure.xcplaygroundpage/Contents.swift
@@ -7,10 +7,31 @@ Also check what `@noescape` means in Swift.
 ****
 Since you now use another function to do the comparison, you can remove the `Comparable` constraint.
 */
+func selectionSort<T>(var array: [T], @noescape isOrderedBefore: (T, T) -> Bool) -> [T] {
+    func minPosition(array: [T], start: Int, @noescape isOrderedBefore: (T, T) -> Bool) -> Int {
+        var minPos = start
+        for i in start..<array.count where isOrderedBefore(array[i], array[minPos]) {
+            minPos = i
+        }
+        return minPos
+    }
 
+    for i in 0..<array.count {
+        let minIndex = minPosition(array, start: i, isOrderedBefore: isOrderedBefore)
+        if i != minIndex {
+            swap(&array[i], &array[minIndex])
+        }
+    }
+
+    return array
+}
+
+
+assert(selectionSort(["c", "b", "d", "a", "A"], isOrderedBefore: <).isSorted())
+assert(selectionSort(["c", "a", "b"], isOrderedBefore: <).isSorted())
     
 
-//assert(selectionSort([3, 1, 2], isOrderedBefore: <).isSorted())
+assert(selectionSort([3, 1, 2], isOrderedBefore: <).isSorted())
 
 
 /*:
