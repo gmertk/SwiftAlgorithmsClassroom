aa1b50c66793420303e1436145d333fe645ca190
Matteo Matassoni <matteo.matassoni@graphitech.it>
2015-11-25 09:11:35 +0100

    turn inner findMinIndex function as method of Array via extension

--- Assignments/Sorting/SelectionSort.playground/Pages/SequenceType Extension.xcplaygroundpage/Contents.swift
+++ Assignments/Sorting/SelectionSort.playground/Pages/SequenceType Extension.xcplaygroundpage/Contents.swift
@@ -7,28 +7,15 @@ Instead of only sorting arrays, modify your function to sort any type conforming
 
 extension SequenceType {
   func selectionSort(@noescape isOrderedBefore: (Generator.Element, Generator.Element) -> Bool) -> [Generator.Element] {
-    func findMinIndex(array: [Generator.Element], startingFrom start: Int = 0, @noescape isOrderedBefore: (Generator.Element, Generator.Element) -> Bool) -> Int? {
-      guard 0..<array.count ~= start else {
-        return nil
-      }
-      
-      var minIndex = start
-      for (index, value) in array[start..<array.count].enumerate() {
-        if isOrderedBefore(value, array[minIndex]) {
-          minIndex = index
-        }
-      }
-      return minIndex + start
-    }
+    var array = Array(self)
     
     // an empty sequence or one that contains just one element is already sorted
-    var array = Array(self)
     guard array.count > 1 else {
       return array
     }
     
     for i in 0..<array.count-1 {
-      if let posmin = findMinIndex(array, startingFrom: i, isOrderedBefore: isOrderedBefore) {
+      if let posmin = array.minIndex(startingFrom: i, isOrderedBefore: isOrderedBefore) {
         if i != posmin {
           swap(&array[i], &array[posmin])
         }
@@ -36,6 +23,22 @@ extension SequenceType {
     }
     
     return array
+  }
+}
+
+extension Array {
+  func minIndex(startingFrom start: Int = 0, @noescape isOrderedBefore: (Generator.Element, Generator.Element) -> Bool) -> Int? {
+    guard 0..<self.count ~= start else {
+      return nil
+    }
+    
+    var minIndex = start
+    for (index, value) in self[start..<self.count].enumerate() {
+      if isOrderedBefore(value, self[minIndex]) {
+        minIndex = index
+      }
+    }
+    return minIndex + start
   }
 }
 
