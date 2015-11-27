bbdfac71e58e04956d752b53d85b3bcf363d3d9f
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2015-11-27 23:12:14 +0100

    Add assignment-2 insertion sort

---
+++ Assignments/Sorting/InsertionSort.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+/*:
+
+## Insertion Sort
+Imagine that you are holding some sorted cards in your hand. You get a new card from the table. You have to insert it in the correct place in your hand. "Take a new card, insert in the right place. Take another new card, insert it in the right place". Here is [a brief explanation of the insertion sort](http://www.sorting-algorithms.com/insertion-sort ).
+
+- [Insertion Sort](Insertion%20Sort)
+
+****
+[Start with the first step](@next)
+*/
