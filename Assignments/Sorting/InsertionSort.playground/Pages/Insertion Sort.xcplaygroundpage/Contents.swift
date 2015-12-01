1b50eb4c44e4a6d12d0c9d42cbd26fc472574178
Alexandre Camilleri <alexandre.camilleri@cgi.com>
2015-12-01 16:34:44 +0100

    Assignment 2

--- Assignments/Sorting/InsertionSort.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
+++ Assignments/Sorting/InsertionSort.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
@@ -8,22 +8,28 @@ Note that there is already a `swap` function in the standard library. However, y
 */
 
 func insertionSort<T>(var array: [T], @noescape isOrderedBefore: (T, T) -> Bool) -> [T] {
-
+    for var i1 = 0; i1 < array.count; ++i1 {
+        for var i2 = 0; i2 < array.count; ++i2 {
+            if i1 != i2 {
+                if isOrderedBefore(array[i1], array[i2]) {
+                    swap(&array[i1], &array[i2])
+                }
+            }
+        }
+    }
     return array
 }
-
-
 //: Test your function with assert. Make sure asserts don't raise any errors. `isSorted` is already defined for you in `Sources/Utilities.swift`. You can add more test cases.
 
 let items = ["c", "d", "b"]
 let sortedItems = insertionSort(items, isOrderedBefore: <)
-//assert(sortedItems.isSorted())
+assert(sortedItems.isSorted())
 assert(items == ["c", "d", "b"]) // double check that items does not change
 
 assert(insertionSort([1], isOrderedBefore: <).isSorted())
 assert(insertionSort([1, 2, 3], isOrderedBefore: <).isSorted())
-//assert(insertionSort([1, 2, 3], isOrderedBefore: >).isSorted(>))
-//assert(insertionSort([3, 2, 1, 2, -1], isOrderedBefore: <).isSorted())
+assert(insertionSort([1, 2, 3], isOrderedBefore: >).isSorted(>))
+assert(insertionSort([3, 2, 1, 2, -1], isOrderedBefore: <).isSorted())
 
 /*:
 [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
